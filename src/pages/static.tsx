b951665708a7e6d7682f022265c54f78d5e5c31c
Josue <josueemcontato@gmail.com>
2023-12-14 16:22:48 -0300

    Add new components and update configuration

--- src/pages/static.tsx
+++ src/pages/static.tsx
@@ -1,68 +1,65 @@
-// pages/static.tsx
+import { GetServerSideProps, GetStaticProps, NextPage } from "next";
+import { ReactNode, useEffect, useState } from "react";
+import { Col, Container, Row } from "reactstrap";
 
-import { GetStaticProps, NextPage } from "next"
-import { useEffect, useState } from "react"
-import { Col, Container, Row } from "reactstrap"
-
-type ApiResponse = {
-  name: string
-  timestamp: Date
+interface ApiResponse {
+    name: string
+    timestamp: Date
 }
 
-export const getStaticProps: GetStaticProps = async()=>{
+export const getStaticProps: GetStaticProps = async () => {
     const staticData = await fetch(`${process.env.NEXT_PUBLIC_APIURL}/api/hello`).then(res => res.json())
 
-    return{
-        props:{
-            staticData 
+    return {
+        props: {
+            staticData
         },
         revalidate: 10
     }
-
 }
 
+const Static: NextPage = (props: {
+    children?: ReactNode
+    staticData?: ApiResponse
+}) => {
+    const [clientSideData, setClientSideDAta] = useState<ApiResponse>()
 
-const Static: NextPage = (
-    props:{
-        children?: React.ReactNode
-        staticData?: ApiResponse
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const fetchData = async () => {
+        const data = await fetch("/api/hello").then(res => res.json())
+        setClientSideDAta(data)
     }
-) => {
-  const [clientSideData, setClientSideData] = useState<ApiResponse>()
 
-  useEffect(() => {
-    fetchData()
-  }, [])
+    return (
+        <Container tag="main">
+            <h1 className="my-5">
+                Como funcionam as renderizações do Next.js
+            </h1>
 
-  const fetchData = async () => {
-    const data = await fetch(`/api/hello`).then(res => res.json())
-    setClientSideData(data)
-  }
+            <Row>
+                <Col>
+                    <h3>
+                        Gerado estaticamente durante o build:
+                    </h3>
+                    <h2>
+                        {props.staticData?.timestamp.toString()}
+                    </h2>
+                </Col>
 
-  return (
-    <Container tag="main">
-      <h1 className="my-5">
-        Como funcionam as renderizações do Next.js
-      </h1>
-
-      <Row>
-        <Col>
-          <h3>
-            Gerado estaticamente durante o build:
-          </h3>
-          <h2>
-            {props.staticData?.timestamp.toString()}
-            </h2>
-        </Col>
-
-        <Col>
-          <h3>
-            Gerado no cliente: {clientSideData?.timestamp.toString()}
-          </h3>
-        </Col>
-      </Row>
-    </Container>
-  )
+                <Col>
+                    <h3>
+                        Gerado no cliente:
+                    </h3>
+                    <h2>
+                        {clientSideData?.timestamp.toString()}
+                    </h2>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
 
 export default Static
---
+++ src/services/products.ts
@@ -0,0 +1,22 @@
+export type ProductType = {
+  id: number
+  name: string
+  description: string
+  price: number
+  imageUrl: string
+  inStock: number
+}
+
+export const fetchProducts = async () => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_APIURL}/api/products`);
+  const data = await response.json();
+  const products: ProductType[] = data.products;
+  return products;
+}
+
+export const fetchProduct = async (id: string | number) => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_APIURL}/api/products/${id}`)
+  const data = await response.json()
+  const product: ProductType = data.product
+  return product
+}
